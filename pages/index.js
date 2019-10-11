ad13df2eb3f28a2f5e11bb164140014338d7a4e1
Nallely <nallely.alcaraz@gmail.com>
2019-10-10 20:30:15 -0700

    fix: attempt to fix duplicate files

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Nav from '../components/Nav'
+import Nav from '../components/nav'
 import Layout from '../containers/Layout'
 import CardNav from '../components/CardNav'
 import HomeCarousel from '../components/HomeCarousel'
