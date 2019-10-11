c263de40712bc2be603c63964f0c360057639879
Nallely <nallely.alcaraz@gmail.com>
2019-10-10 20:57:30 -0700

    fix: layout import path

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../containers/Layout'
+import Layout from '../containers/layout'
 
 const About = () => {
     return <Layout>About</Layout>
--- pages/pricing.js
+++ pages/pricing.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../containers/Layout'
+import Layout from '../containers/layout'
 
 const Pricing = () => {
     return <Layout>Pricing</Layout>
