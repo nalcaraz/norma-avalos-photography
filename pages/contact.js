3e467e107a76c10bf3b4868b464607c9ecbe2d62
Nallely <nallely.alcaraz@gmail.com>
2019-10-15 20:36:03 -0700

    fix: netlify form

--- pages/contact.js
+++ pages/contact.js
@@ -43,7 +43,7 @@ const Contact = () => {
     }, [email, message])
     return (
         <Layout>
-            <form netlify="true" name="contact-norma-avalos-photography">
+            <form data-netlify="true" method="POST" name="contact-norma-avalos-photography">
                 <div className="form-group" noValidate>
                     <label htmlFor="email" className="font-cinzel">
                         Email address
