43fcce19eedeb5d2da0e42bcd4b99d768aef5d3a
Nallely <nallely.alcaraz@gmail.com>
2019-10-15 20:29:27 -0700

    feature: add contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../containers/layout'
+import * as yup from 'yup'
+
+let schema = yup.object().shape({
+    email: yup
+        .string()
+        .required('Email address required')
+        .email('Enter a valid email address'),
+    message: yup.string().required('Message required'),
+})
 
 const Contact = () => {
-    return <Layout>Contact</Layout>
+    const [email, setEmail] = useState()
+    const [message, setMessage] = useState()
+    const [formValid, setFormValid] = useState(false)
+    const [formDirty, setFormDirty] = useState(false)
+    const [error, setError] = useState()
+
+    const handleEmailChange = e => {
+        setFormDirty(true)
+        setEmail(e.target.value)
+    }
+
+    const handleMessageChange = e => {
+        setFormDirty(true)
+        setMessage(e.target.value)
+    }
+
+    useEffect(() => {
+        // check validity
+        schema
+            .validate({
+                email,
+                message,
+            })
+            .then(function(valid) {
+                setFormValid(valid)
+            })
+            .catch(err => {
+                setError(err)
+            })
+    }, [email, message])
+    return (
+        <Layout>
+            <form netlify="true">
+                <div className="form-group" noValidate>
+                    <label htmlFor="email" className="font-cinzel">
+                        Email address
+                    </label>
+                    <input
+                        onChange={e => handleEmailChange(e)}
+                        type="email"
+                        className="form-control"
+                        id="email"
+                        aria-describedby="emailHelp"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message" className="font-cinzel">
+                        Message
+                    </label>
+                    <textarea
+                        onChange={e => handleMessageChange(e)}
+                        className="form-control"
+                        id="message"
+                        rows="3"
+                    ></textarea>
+                </div>
+                {!formValid && formDirty && (
+                    <p className="text-danger">{error.message}</p>
+                )}
+                <div className="d-flex justify-content-center">
+                    <button
+                        disabled={!formValid}
+                        type="submit"
+                        className="btn btn-secondary font-cinzel align-self-center"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </Layout>
+    )
 }
 
 export default Contact
