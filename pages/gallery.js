05a1df564f3f9227e3d41bd20504b637e3bf8657
Nallely <nallely.alcaraz@gmail.com>
2019-10-19 20:57:52 -0700

    fix:remove gallery inital props

--- pages/gallery.js
+++ pages/gallery.js
@@ -6,12 +6,7 @@ import fetch from 'isomorphic-unfetch'
 
 
 const Gallery = ({stuff}) => {
-console.log("props", stuff)
-    useEffect(()=>{
-        var getUrl = window.location;
-    var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-    console.log("baseUrl", baseUrl)
-    })
+
     return (
         <Layout title="Gallery">
           <h1 className="dawn-new-day text-center">Gallery</h1>
@@ -35,24 +30,5 @@ console.log("props", stuff)
         </Layout>
     )
 }
-
-Gallery.getInitialProps = async ({ req }) => {
-    console.log("process.env.AIRTABLE_API", process.env.AIRTABLE_API)
-    console.log("req", req)
-    const basePath = (process.env.NODE_ENV === 'development') ? 'http://localhost:3000' : 'https://yourdomain.com'
-//    const res = await fetch("https://api.airtable.com/v0/appxq7iTOEHVbUIdg/Gallery?maxRecords=3&view=Grid%20view", {
-//         method: "GET",
-//         headers: {"Authorization": `Bearer keyBihNAgWRI2eMPP`,
-//                   "Content-Type": "application/json"},
-    
-//       })
-     
-     // .then((res) =>  res)
-      //.catch(error => (error))
-     const res = await fetch(basePath +'/.netlify/functions/gallery')
-     const json = await res.json()
-   console.log("response", json)
-   return { stuff: json }
-  }
 
 export default Gallery
