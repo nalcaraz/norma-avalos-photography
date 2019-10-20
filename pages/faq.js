5abd3c891fc65dc0b9a8ebc8c11732b09f704fc7
Nallely <nallely.alcaraz@gmail.com>
2019-10-19 21:45:09 -0700

    fix: remove getInitialProps to use instead useEffect

--- pages/faq.js
+++ pages/faq.js
@@ -1,46 +1,79 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../containers/layout';
-const fetch = require('node-fetch').default;
+//const fetch = require('node-fetch').default;
 
-const Faq = ({ records }) => {
-    console.log('records', records);
-    const { records: faqs } = records;
+const Faq = () => {
+    const [faqs, setFaqs] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`/.netlify/functions/faq`)
+            .then(function(response) {
+                if (!response.ok) return Promise.reject(response);
+                return response.json();
+            })
+            .then(resp => {
+                setFaqs(resp.records);
+                setIsLoading(false);
+            })
+            .catch(function(error) {
+                setIsLoading(false);
+
+                console.log(' error: ', error);
+            });
+    }, []);
+
     return (
         <Layout title="FAQS">
             <h1 className="dawn-new-day text-center">FAQs</h1>
             <div id="faqs">
-                {faqs &&
+                {!isLoading &&
+                    faqs &&
                     faqs.length > 0 &&
                     faqs.map(q => {
                         return (
                             <div className="card" key={q.fields.QuestionNumber}>
-                            <div className="card-header">
-                             {q.fields.Question}
+                                <div className="card-header">
+                                    {q.fields.Question}
+                                </div>
+                                <div className="card-body">
+                                    <p className="card-text">
+                                        {q.fields.Answer}
+                                    </p>
+                                </div>
                             </div>
-                            <div className="card-body">
-                              <p className="card-text">{q.fields.Answer}</p>
-                            </div>
-                          </div>
                         );
                     })}
-                {(!faqs || faqs.length == 0) && (
+                {!isLoading && (!faqs || faqs.length == 0) && (
                     <p>Please contact me if you have questions</p>
+                )}
+                {isLoading && (
+                    <div className="progress">
+                        <div
+                            className="progress-bar bg-success progress-bar-striped progress-bar-animated"
+                            role="progressbar"
+                            aria-valuenow="75"
+                            aria-valuemin="0"
+                            aria-valuemax="100"
+                            style={{ width: '75%' }}
+                        ></div>
+                    </div>
                 )}
             </div>
         </Layout>
     );
 };
 
-Faq.getInitialProps = async ({ req }) => {
-    const baseUrl =
-        process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3000'
-            : 'https://normaavalosphotography.netlify.com';
+// Faq.getInitialProps = async ({ req }) => {
+//     const baseUrl =
+//         process.env.NODE_ENV === 'development'
+//             ? 'http://localhost:3000'
+//             : 'https://normaavalosphotography.netlify.com';
 
-    const res = await fetch(baseUrl + '/.netlify/functions/faq');
-    const json = await res.json();
+//     const res = await fetch(baseUrl + '/.netlify/functions/faq');
+//     const json = await res.json();
 
-    return { records: json };
-};
+//     return { records: json };
+// };
 
 export default Faq;
