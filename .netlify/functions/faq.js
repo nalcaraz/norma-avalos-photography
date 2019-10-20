18066b2b21d9f112c7d20b40a6cc12d30011aacc
Nallely <nallely.alcaraz@gmail.com>
2019-10-19 20:48:07 -0700

    feature: add netlify functions to connect to api

---
+++ .netlify/functions/faq.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch').default
+const dotenv = require('dotenv')
+dotenv.config()
+
+exports.handler = function(event, context, callback) {
+    return fetch(
+        'https://api.airtable.com/v0/appxq7iTOEHVbUIdg/FAQ?maxRecords=3&view=Grid%20view&filterByFormula=AND(Question!%3D%22%22%2CAnswer!%3D%22%22%2CIsPublished%3D1)',
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${process.env.AIRTABLE_API}`,
+                'Content-Type': 'application/json',
+            },
+        }
+    )
+        .then(res => {
+            return res.json()
+        })
+        .then(data => ({
+            statusCode: 200,
+            body: JSON.stringify(data),
+        }))
+        .catch(err => {
+            return { statusCode: 422, body: String(err) }
+        })
+}
