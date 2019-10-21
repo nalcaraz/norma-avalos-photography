3667abe08482427dd2e3b43f7a018207d1234c3f
Nallely <nallely.alcaraz@gmail.com>
2019-10-20 17:24:35 -0700

    refactor(praises): remove getInitial props to set functions first

--- pages/praises.js
+++ pages/praises.js
@@ -3,42 +3,43 @@ import Layout from '../containers/layout';
 const fetch = require('node-fetch').default;
 
 const Praises = ({ records }) => {
-    const { records: praises } = records;
-    return (
-        <Layout title="Praises">
-            <h1 className="dawn-new-day text-center">Praises</h1>
-            {praises &&
-                praises.length > 0 &&
-                praises.map((q, i) => {
-                    return (
-                        <div className="card m-2" key={i}>
-                            <div className="card-body">
-                                <h5 className="font-cinzel">
-                                    {q.fields.Praise}
-                                </h5>
-                                <small className="card-text font-cinzel ">
-                                    -<i>{q.fields.PraiserName} {q.fields.DateOfPraise && `, ${q.fields.DateOfPraise}`}</i>
-                                </small>
-                            </div>
-                        </div>
-                    );
-                })}
-            {(!praises || praises.length == 0) && (
-                <p className="font-cinzel text-center">Please contact me if you would like to leave a review</p>
-            )}
-        </Layout>
-    );
+    // const { records: praises } = records;
+    // return (
+    //     <Layout title="Praises">
+    //         <h1 className="dawn-new-day text-center">Praises</h1>
+    //         {praises &&
+    //             praises.length > 0 &&
+    //             praises.map((q, i) => {
+    //                 return (
+    //                     <div className="card m-2" key={i}>
+    //                         <div className="card-body">
+    //                             <h5 className="font-cinzel">
+    //                                 {q.fields.Praise}
+    //                             </h5>
+    //                             <small className="card-text font-cinzel ">
+    //                                 -<i>{q.fields.PraiserName} {q.fields.DateOfPraise && `, ${q.fields.DateOfPraise}`}</i>
+    //                             </small>
+    //                         </div>
+    //                     </div>
+    //                 );
+    //             })}
+    //         {(!praises || praises.length == 0) && (
+    //             <p className="font-cinzel text-center">Please contact me if you would like to leave a review</p>
+    //         )}
+    //     </Layout>
+    //);
+    return <p>Praises</p>
 };
 
-Praises.getInitialProps = async ({ req }) => {
-    const baseUrl =
-        process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3000'
-            : 'https://normaavalosphotography.netlify.com';
+// Praises.getInitialProps = async ({ req }) => {
+//     const baseUrl =
+//         process.env.NODE_ENV === 'development'
+//             ? 'http://localhost:3000'
+//             : 'https://normaavalosphotography.netlify.com';
 
-    const res = await fetch(baseUrl + '/.netlify/functions/praises');
-    const json = await res.json();
+//     const res = await fetch(baseUrl + '/.netlify/functions/praises');
+//     const json = await res.json();
 
-    return { records: json };
-};
+//     return { records: json };
+// };
 export default Praises;
